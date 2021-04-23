a2f3c7e07273b5e84f88e837f1186331a1897b9f
mrganeshkudale <mrganeshkudale@gmail.com>
2021-04-23 12:20:16 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -23,5 +23,5 @@
 
 export const environment = {
   production: false,
-  REST_API_URL: 'http://tnt-aks-automator.eastus.cloudapp.azure.com:9090'
+  REST_API_URL: 'http://petclinicapi.e46708b92c054086909b.eastus.aksapp.io/petclinic/api'
 };
